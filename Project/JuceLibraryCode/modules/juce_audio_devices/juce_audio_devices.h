5162cf5e20b2d3967384e967159cf968d182596a
Joshua <joshofreeness@gmail.com>
2015-06-19 12:36:35 +1200

    Made the windows version point to the linux juce modules because the introjucer decided to make everything point to what the windows version was pointing to, still works on windows because we have source files

--- Project/JuceLibraryCode/modules/juce_audio_devices/juce_audio_devices.h
+++ Project/JuceLibraryCode/modules/juce_audio_devices/juce_audio_devices.h
@@ -1,11 +1,5 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#if defined (JUCER_VS2013_78A5020)
- #include "../../../../../JUCE-WINDOWS/modules/juce_audio_devices/juce_audio_devices.h"
-#elif defined (JUCER_LINUX_MAKE_7346DA2A)
- #include "../../../../JUCE-LINUX/modules/juce_audio_devices/juce_audio_devices.h"
-#else
- #error "This file is designed to be used in an Introjucer-generated project!"
-#endif
+#include "../../../../JUCE-LINUX/modules/juce_audio_devices/juce_audio_devices.h"
 
