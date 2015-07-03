13855fa2b68764711db40a9c731af9df462f6526
Joshua <joshofreeness@gmail.com>
2015-07-03 14:19:20 +1200

    Populated the keys in the enum

--- Project/Source/Enums.h
+++ Project/Source/Enums.h
@@ -12,9 +12,9 @@
 #define ENUMS_H_INCLUDED
 
 //TODO: Idk, are there more?
-enum class Key_t { A, B, C, D, E, F, G };
+enum class Key_t { A, A_sharp, B, C, C_sharp, D, D_sharp, E, F, F_sharp, G, G_sharp };
 
-//TODO: Fill this with actual values.
+//TODO: Fill this with actual values. (Numbers 1-7 not sure if enum is necesarry)
 enum class Chord_t { Placeholder1, Placeholder2, Placeholder3 };
 
 
