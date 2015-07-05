bb7afe22dc2c0bd7a36538690e4c298eb36de7d4
Michael Lo <nz.ampersand@gmail.com>
2015-07-05 13:10:59 +1200

    Got some timing stuff working! Still no idea what to do about on/off messages, but can play single notes at any time in the future.

--- Project/Source/Output/OutputController.cpp
+++ Project/Source/Output/OutputController.cpp
@@ -10,35 +10,57 @@
 
 #include "OutputController.h"
 
-OutputController::OutputController(){}
+OutputController::OutputController(){
+	midiOutput->startBackgroundThread();
+}
 OutputController::~OutputController(){}
 
 
+void OutputController::ClockProcess(MidiBuffer& midiMessages) {
+	//New code done with lots of help from http://www.juce.com/forum/topic/midi-accuracy, particularly comment #12
 
-void OutputController::ClockProcess(juce::MidiBuffer& midiMessages) {
+	if (test) {
+		PlayNote(1050.0f, 1.0f, 10, midiMessages, 100);
+		PlayNote(950.0f, 1.0f, 10, midiMessages, 200);
+		PlayNote(850.0f, 1.0f, 10, midiMessages, 300);
+		PlayNote(750.0f, 1.0f, 10, midiMessages, 400);
+		PlayNote(650.0f, 1.0f, 10, midiMessages, 500);
+		PlayNote(550.0f, 1.0f, 10, midiMessages, 600);
+		PlayNote(450.0f, 1.0f, 10, midiMessages, 700);
+		PlayNote(350.0f, 1.0f, 10, midiMessages, 800);
+		PlayNote(250.0f, 1.0f, 10, midiMessages, 900);
+		PlayNote(150.0f, 1.0f, 10, midiMessages, 1000);
+		PlayNote(50.0f, 1.0f, 10, midiMessages, 1100);
+		PlayNote(5.0f, 1.0f, 10, midiMessages, 1200);
+		PlayNote(0.1f, 1.0f, 10, midiMessages, 1300);
+		PlayNote(0.01f, 1.0f, 10, midiMessages, 1400);
+		PlayNote(0.001f, 1.0f, 10, midiMessages, 1500);
 
+		test = false;
+	}
+
+
+	/*
 	if (first)
 	{
-		PlayNote(450.0f, 1.0f, 1);
+		PlayNote(450.0f, 1.0f, 1000);
 		first = false;
 		second = true;
 	}
 	else if (second)
 	{
-		PlayNote(300.0f, 1.0f, 30000);
+		PlayNote(300.0f, 1.0f, 3000);
 		second = false;
 	}
 	else
 	{
-		//PlayNote(150.0f, 1.0f, 10000);
+		PlayNote(150.0f, 1.0f, 1000);
 	}
 
 	std::list<MidiMessage*>::const_iterator iterator;
 
-	int i = 1;
 	for (iterator = scheduledToAddToBuffer.begin(); iterator != scheduledToAddToBuffer.end(); ++iterator) {
-		midiMessages.addEvent(*(*iterator), i);
-		i++;
+		midiMessages.addEvent(*(*iterator), midiMessages.getLastEventTime());
 	}
 
 	scheduledToAddToBuffer.clear();
@@ -46,11 +68,26 @@ void OutputController::ClockProcess(juce::MidiBuffer& midiMessages) {
 	//reset pointers (possible race condition)
 	currentOnList = headOnList;
 	currentOffList = headOffList;
+	*/
 }
 
 //This method will schedule a note to be played
-void OutputController::PlayNote(float hertz, float volume, double length)
+void OutputController::PlayNote(float hertz, float volume, double length, MidiBuffer& midiMessages, int delay)
 {
+	//https://en.wikipedia.org/wiki/MIDI_Tuning_Standard
+	int midiNoteValue = 69 + (12 * log2f(hertz / 440.0f)); //midi note from provided frequency
+
+	MidiMessage midiMessage = MidiMessage::noteOn(1, midiNoteValue, volume);
+	midiMessage.setTimeStamp(Time::getMillisecondCounter());
+
+	double aBpmRatio = (double)60 / 120; //the divisor here is the bpm
+	
+	midiMessages.addEvent(midiMessage, unit * aBpmRatio);
+
+	double time = Time::getMillisecondCounter() + delay; // The time at which the note is to be played. Time::getMillisecondCounter() = current time.
+	midiOutput->sendBlockOfMessages(midiMessages, time, unit);
+
+	/*
 	MidiMessage* midiMessageNoteOn = GetNextFromList(headOnList, currentOnList, true);
 	MidiMessage* midiMessageNoteOff = GetNextFromList(headOffList, currentOffList, false);
 
@@ -60,15 +97,16 @@ void OutputController::PlayNote(float hertz, float volume, double length)
 	midiMessageNoteOn->setVelocity(volume);
 	midiMessageNoteOn->setChannel(1);
 	midiMessageNoteOn->setNoteNumber(midiNoteValue);
-	midiMessageNoteOn->setTimeStamp(0);
+	midiMessageNoteOn->setTimeStamp(Time::getMillisecondCounter());
 
 	midiMessageNoteOff->setVelocity(volume);
 	midiMessageNoteOff->setChannel(1);
 	midiMessageNoteOff->setNoteNumber(midiNoteValue);
-	midiMessageNoteOff->setTimeStamp(length);
+	midiMessageNoteOff->setTimeStamp(Time::getMillisecondCounter() +  length);
 
 	scheduledToAddToBuffer.push_back(midiMessageNoteOn);
 	//scheduledToAddToBuffer.push_back(midiMessageNoteOff);
+	*/
 }
 
 MidiMessage* OutputController::GetNextFromList(midiNode *head, midiNode *current, bool noteOnMessage)
--- Project/Source/Output/OutputController.h
+++ Project/Source/Output/OutputController.h
@@ -16,6 +16,7 @@
 
 namespace juce{
 	class MidiBuffer;
+	class MidiMessage;
 }
 
 struct midiNode
@@ -29,8 +30,8 @@ public:
 	OutputController();
 	~OutputController();
 
-	void ClockProcess(juce::MidiBuffer& midiMessages);
-	void PlayNote(float hertz, float velocity, double length);
+	void ClockProcess(MidiBuffer& midiMessages);
+	void PlayNote(float hertz, float velocity, double length, juce::MidiBuffer& midiMessages, int delay);
 private:
 	midiNode *headOnList = nullptr;
 	midiNode *headOffList = nullptr;
@@ -41,9 +42,14 @@ private:
 
 	bool first = true;
 	bool second = false;
+	bool test = true;
+
+	const double unit = 44100.0; // Represents something to do with the number of channels in the sound driver, I think. Most common values 44,100 and 44,400. Idk. Google it.
 
 	MidiMessage* GetNextFromList(midiNode* head, midiNode* current, bool onMessage);
 	MidiMessage NewMidiMessage(bool noteOnMessage);
+
+	MidiOutput* midiOutput = MidiOutput::openDevice(0);
 
 };
 
