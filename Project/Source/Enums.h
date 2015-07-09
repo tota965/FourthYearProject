3ded643c4f8158223d0d7f44dff40ad893e5cccf
Michael Lo <nz.ampersand@gmail.com>
2015-07-09 15:29:29 +1200

    Populated Chord enum, added comment explaining reasoning behind keeping it. Will clean up comments later.

--- Project/Source/Enums.h
+++ Project/Source/Enums.h
@@ -15,7 +15,10 @@
 enum class Key_t { A, A_sharp, B, C, C_sharp, D, D_sharp, E, F, F_sharp, G, G_sharp };
 
 //TODO: Fill this with actual values. (Numbers 1-7 not sure if enum is necesarry)
-enum class Chord_t { Placeholder1, Placeholder2, Placeholder3 };
+// Not necessary, but I think it'd be worth it because we can ensure no instance ever 
+// leaves that range and also makes the code more readable - 
+// don't have to go look up what the number represents.
+enum class Chord_t { 1, 2, 3, 4, 5, 6, 7 };
 
 
 #endif  // ENUMS_H_INCLUDED
