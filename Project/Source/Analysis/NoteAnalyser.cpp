f8e79b4d46dd9aebd5bd9b8ced91eaee9869fc60
Michael Lo <nz.ampersand@gmail.com>
2015-07-27 16:23:45 +1200

    Added placeholder files for the 'brain' section of our project.

---
+++ Project/Source/Analysis/NoteAnalyser.cpp
@@ -0,0 +1,16 @@
+/*
+  ==============================================================================
+
+    NoteAnalyser.cpp
+    Created: 27 Jul 2015 4:21:34pm
+    Author:  Michael
+
+  ==============================================================================
+*/
+
+#include "NoteAnalyser.h"
+
+NoteAnalyser::NoteAnalyser(){}
+NoteAnalyser::~NoteAnalyser(){}
+
+//TODO: Sort out the list of functions to go here and start writing them.
