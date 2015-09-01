512f59428f977bc05a297685c3c4477e1bcfdb93
Joshua <joshofreeness@gmail.com>
2015-09-01 15:58:34 +1200

    Adding equation to figure out what note of the scale a frequency is

--- Project/Source/Analysis/SenseMaker.cpp
+++ Project/Source/Analysis/SenseMaker.cpp
@@ -9,6 +9,7 @@
 */
 
 #include "SenseMaker.h"
+#include <math.h>
 
 SenseMaker::SenseMaker(){}
 
@@ -73,6 +74,36 @@ void SenseMaker::setKeyTonic(int key)
 
 int SenseMaker::frequencyToNoteInKey(double freq)
 {
+	/*
+		fn = f0 * (a)n 
+		where
+		f0 = the frequency of one fixed note which must be defined. A common choice is setting the A above middle C (A4) at f0 = 440 Hz.
+		n = the number of half steps away from the fixed note you are. If you are at a higher note, n is positive. If you are on a lower note, n is negative.
+		fn = the frequency of the note n half steps away.
+		a = (2)1/12 = the twelth root of 2 = the number which when multiplied by itself 12 times equals 2 = 1.059463094359...
+		http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html
+	*/
+	if (freq == currentKeyTonic){return 0;} 
+	
+	int currentSteps = 0;
+	if (freq > currentKeyTonic)
+	{
+		
+		double aValue = pow(2, 1.0 / 12.0);
+		double multiplier = aValue;
+
+		while (currentKeyTonic * multiplier < freq )
+		{
+			currentSteps++;
+			multiplier *= aValue;
+		}
+	}
+	else
+	{
+		//TODO: Implement if the freq is less than the tonic
+	}
+
+	//It is in the scale if it is 2, 4, 5, 7, 9, 11, 12
 	return 0;
 }
 
