c5282bb8117aefd41b258a178a40cd92f16a306e
Joshua <joshofreeness@gmail.com>
2015-09-04 11:38:41 +1200

    Added the class to controll the brain and make decisions based on info from sensemaker and markov information

---
+++ Project/Source/Analysis/BrainController.cpp
@@ -0,0 +1,16 @@
+/*
+  ==============================================================================
+
+    BrainController.cpp
+    Created: 4 Sep 2015 11:35:56am
+    Author:  Joshua
+
+  ==============================================================================
+*/
+
+#include "BrainController.h"
+
+BrainController::BrainController(){}
+
+BrainController::~BrainController(){}
+
---
+++ Project/Source/Analysis/BrainController.h
@@ -0,0 +1,24 @@
+/*
+  ==============================================================================
+
+    BrainController.h
+    Created: 4 Sep 2015 11:35:56am
+    Author:  Joshua
+
+  ==============================================================================
+*/
+
+#ifndef BRAINCONTROLLER_H_INCLUDED
+#define BRAINCONTROLLER_H_INCLUDED
+
+class BrainController
+{
+public:
+	BrainController();
+	~BrainController();
+private:
+};
+
+
+
+#endif  // BRAINCONTROLLER_H_INCLUDED
